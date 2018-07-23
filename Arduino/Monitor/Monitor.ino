b1088798d41abef2639ed2ecc468403eac1a1514
jimmyex500sl <jimmyex500sl@gmail.com>
2018-07-24 01:04:21 +0800

    新增未測試的Ph值與水位程式與相對應的node檔
    更變檔案名稱為Monitor
    新增水位副程式//untest
    新增ph值副程式//untest
    更動node接收數值的程式//untest

---
+++ Arduino/Monitor/Monitor.ino
@@ -0,0 +1,169 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <dht11.h>
+#include <Streaming.h>
+
+#define SensorPin A2            //pH meter Analog output to Arduino Analog Input 0
+#define Offset 0.00    
+#define ArrayLenth  40    //times of collection
+int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
+int pHArrayIndex=0;   
+
+dht11 DHT11;
+
+const byte dataPin = 8;
+const int SW[3]={7,6,5};
+
+unsigned long past = 0; 
+const unsigned long interval = 2 * 1000L;
+
+byte mac[] = {0x00,0xa0,0x96,0x7b,0x87,0xb3};
+
+// 要連接的伺服器IP位址，這你們要給我
+IPAddress server(192, 168, 0, 196);
+
+// 本機的IP位址，也是你們要給我
+IPAddress ip(192, 168, 0, 200);
+
+// 初始化乙太用戶端
+EthernetClient client;
+
+void setup() {
+  Serial.begin(9600);
+  for(int i = 0;i < 3; i++)
+  {
+pinMode(SW[i],INPUT);//指撥開關腳位水位
+}
+Ethernet.begin(mac, ip);  // 初始化乙太網路連線
+
+// 等待一秒鐘，讓乙太網路卡有時間進行初始化
+delay(1000);
+Serial.println("connecting...");
+}
+
+void loop() {
+  if (millis() - past > interval) {
+    int chk = DHT11.read(dataPin);
+    int moisture = analogRead(A0);
+
+    if (chk == 0) {
+      httpSend(moisture, calPh(), waterLevel());
+    } else {
+      Serial.println("Sensor Error");
+    }
+  }
+}
+
+//untest ph值副程式
+double calPh(){
+  int voltage;
+
+  pHArray[pHArrayIndex++] = analogRead(SensorPin);
+  if(pHArrayIndex == ArrayLenth){
+    pHArrayIndex = 0;
+  }
+  voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
+
+  return voltage;
+}
+
+//un test ph值所需副程式
+double avergearray(int* arr, int number){
+  int i;
+  int max,min;
+  double avg;
+  long amount = 0;
+  if(number <= 0){
+    Serial.println("Error number for the array to avraging!/n");
+    return 0;
+  }
+  if(number < 5){   //less than 5, calculated directly statistics
+    for(i = 0; i < number; i++){
+      amount+=arr[i];
+    }
+    avg = amount/number;
+    return avg;
+  }else{
+    if(arr[0] < arr[1]){
+      min = arr[0];
+      max = arr[1];
+    }
+    else{
+      min = arr[1];
+      max = arr[0];
+    }
+    for(i = 2; i < number; i++){
+      if(arr[i] < min){
+        amount+=min;        //arr<min
+        min = arr[i];
+      }else {
+        if(arr[i] > max){
+          amount+=max;    //arr>max
+          max = arr[i];
+        }else{
+          amount+=arr[i]; //min<=arr<=max
+        }
+      }//if
+    }//for
+    avg = (double)amount/(number-2);
+  }//if
+  return avg;
+}
+//untest 水位副程式
+String waterLevel(){
+  int a,b,c;
+
+  
+  a=digitalRead(SW[0]);
+  b=digitalRead(SW[1]);
+  c=digitalRead(SW[2]);
+
+//Serial.print(a);
+//Serial.print(b);
+//Serial.print(c);
+//Serial.print("\n");
+
+  if (a==0 && b==0 && c==0){
+//Serial.print("FULL");
+    return "FULL";      
+  }
+  if (a==0 && b==0 && c==1){
+//Serial.print("2");
+    return "2";
+  }
+  if (a==0&&b==1&&c==1){
+//Serial.print("1");
+    return "1";
+  }
+  if (a==1 && b==1 && c==1){
+//Serial.print("EMPTY");
+    return "EMPTY";
+  }
+//Serial.print("\n");
+}
+
+void httpSend(int moisture, double voltage ,String waterLevel) {
+  char temperatureBuffer[6] = "";
+  char humidityBuffer[6] = "";
+  char soidmoistureBuffer[6] = "";
+  char phBuffer[6] = "";
+  
+  client.stop();
+
+// 連線到指定伺服器的5438埠號
+  if (client.connect(server, 5438)) {
+    Serial.println("connected");
+// 發送HTTP請求
+    client << "GET /th?airtemperature=" << dtostrf(DHT11.temperature, 5, 2, temperatureBuffer)
+           << "&airhumidity=" << dtostrf(DHT11.humidity, 5, 2, humidityBuffer)
+           << "&soidmoisture=" << dtostrf(moisture, 5, 2, soidmoistureBuffer)
+           << "&ph=" << dtostrf(3.5*voltage + Offset, 5, 3, phBuffer)//untest
+           << "&waterLevel=" << waterLevel//untest
+           << " HTTP/1.1\n";
+    client.println();
+
+    past = millis();
+  } else {
+    Serial.println("connection failed");
+  }
+}
--- node/index.js
+++ node/index.js
@@ -6,14 +6,16 @@ app.get("/", function(req, res) {
 });
 
 app.get("/th", function(req, res) {
-  var temp = req.query.t;   // 讀取查詢字串的t值
-  var humid = req.query.h;  // 讀取查詢字串的h值
-  var aaa = req.query.aaa;
+  var temp = req.query.airtemperature;   // 讀取查詢字串的t值
+  var humid = req.query.airtemperature;  // 讀取查詢字串的h值
+  var soidhumid = req.query.soidmoisture;
+  var ph = req.query.ph;
+  var waterLevel = req.query.waterLevel;
 
   // 確認有收到溫度和濕度值（兩者都不是undefined）
   if (temp != undefined && humid != undefined ) {
-　　console.log("溫度: " + temp + "，濕度： " + humid+",aaa "+aaa);
-　　res.send("溫度: " + temp + "°C，濕度： " + humid +"%"+",aaa "+aaa);
+　　console.log("溫度: " + temp + "，濕度： " + humid + ",土地濕度 " + soidhumid + "PH值" + ph + "水位" + waterLevel);
+　　res.send("溫度: " + temp + "°C，濕度： " + humid + "%" + ",土地濕度 "+soidhumid + "PH值" + ph + "水位" + waterLevel););
   } else {
   	console.log("沒收到資料！");
   }
