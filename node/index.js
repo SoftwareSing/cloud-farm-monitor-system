d2a8d6d2ac0c9b57cc8fe24eff4c84b836d88596
jimmyex500sl <jimmyex500sl@gmail.com>
2018-07-24 19:27:39 +0800

    修正var

--- node/index.js
+++ node/index.js
@@ -1,30 +1,30 @@
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 
 app.get("/", function(req, res) {
-	res.send("arduino資訊網頁");
+    res.send("arduino資訊網頁");
 });
 
 app.get("/th", function(req, res) {
-  var temp = req.query.airtemperature;   // 讀取查詢字串的t值
-  var humid = req.query.airhumidity;  // 讀取查詢字串的h值
-  var soidhumid = req.query.soidmoisture;
-  var ph = req.query.ph;
-  var waterLevel = req.query.waterLevel;
+    const temp = req.query.airtemperature; // 讀取查詢字串的t值
+    const humid = req.query.airhumidity; // 讀取查詢字串的h值
+    const soidhumid = req.query.soidmoisture;
+    const ph = req.query.ph;
+    const waterLevel = req.query.waterLevel;
 
-  // 確認有收到溫度和濕度值（兩者都不是undefined）
-  if (temp != undefined && humid != undefined ) {
-　　console.log("溫度 :" + temp + "，濕度 :" + humid + ",土地濕度 :" + soidhumid + " PH值 :" + ph + " 水位 :" + waterLevel);
-　　res.send("溫度: " + temp + "°C，濕度： " + humid + "%" + ",土地濕度 "+soidhumid + "PH值" + ph + "水位" + waterLevel);
-  } else {
-  	console.log("沒收到資料！");
-  }
+    // 確認有收到溫度和濕度值（兩者都不是undefined）
+    if (temp !== undefined && humid !== undefined) {
+        console.log("溫度 :" + temp + "，濕度 :" + humid + ",土地濕度 :" + soidhumid + " PH值 :" + ph + " 水位 :" + waterLevel);
+        res.send("溫度: " + temp + "°C，濕度： " + humid + " % ,土地濕度 " + soidhumid + "PH值" + ph + "水位" + waterLevel);
+    } else {
+        console.log("沒收到資料！");
+    }
 });
 
-app.use("*",function(req,res){
-  res.status(404).send('查無此頁！');
+app.use("*", function(req, res) {
+    res.status(404).send("查無此頁！");
 });
 
-var server = app.listen(5438, function () {
-  console.log("網站伺服器在5438埠口開工了！");
+const server = app.listen(5438, function() {
+    console.log("網站伺服器在5438埠口開工了！");
 });
