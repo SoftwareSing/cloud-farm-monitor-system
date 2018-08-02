6d71e0451406158a50241bdc0e42ffbd8e5b4b05
jimmyex500sl <jimmyex500sl@gmail.com>
2018-08-03 00:33:50 +0800

    修正排版與優化
    修正arduino的排版
    優化水位程式
    修正index.js的收值
    control.js不用管下次的commit在修正

--- node/control.js
+++ node/control.js
@@ -3,114 +3,113 @@ const firmata = require("firmata");
 const five = require("johnny-five");
 
 const sp = new VirtualSerialPort({
-  host: "172.23.99.10"
+    host: "172.23.99.10"
 });
 console.log("hi");
 const io = new firmata.Board(sp);
 console.log("hi again");
 const board = new five.Board({io: io, repl: true});
 
-function back(point , water, fertilizer, LED){
-
+function back(point, water, fertilizer, LED) {
 
 }
 
+function go(point, water, fertilizer, LED) {
+    // const x = point.x;
+    const {x, y, z} = point;
+    io.once("ready", function() {
+        console.log("fuck");
+        console.log("IO Ready");
+        io.isReady = true;
 
-function go(point , water, fertilizer, LED) {
-  // const x = point.x;
-  const {x, y, z} = point;
-  io.once("ready", function(){
-   console.log("fuck");
-   console.log("IO Ready");
-   io.isReady = true;
+        board.on("ready", function() {
+            console.log("five ready");
 
-   board.on("ready", function(){
+            //var led = new five.Led();
+            const stopButtonX = new five.Button({
+                pin: 3,
+                invert: true
+            });
+            const stopButtonY = new five.Button({
+                pin: 14,
+                invert: true
+            });
+            const stopButtonZ = new five.Button({
+                pin: 18,
+                invert: true
+            });
 
+            const stepperX = new five.Stepper({
+                type: five.Stepper.TYPE.DRIVER,
+                stepsPerRev: 500,
+                pins: {
+                    step: 54,
+                    dir: 55
+                }
+            });
+            const stepperY = new five.Stepper({
+                type: five.Stepper.TYPE.DRIVER,
+                stepsPerRev: 500,
+                pins: {
+                    step: 26,
+                    dir: 28
+                }
+            });
+            const stepperZ = new five.Stepper({
+                type: five.Stepper.TYPE.DRIVER,
+                stepsPerRev: 200,
+                pins: {
+                    step: 36,
+                    dir: 34
+                }
+            });
 
-    console.log("five ready");
+            const ledR = new five.Led(17);
+            const ledG = new five.Led(23);
+            const ledB = new five.Led(16);
 
-        //var led = new five.Led();
-        let stopButton_X = new five.Button({
-           pin: 3, 
-           invert: true
-       });
-       let stopButton_Y = new five.Button({
-           pin: 14, 
-           invert: true
-       });
-       let stopButton_Z = new five.Button({
-           pin: 18, 
-           invert: true
-       });
+            const waterPumprelay = new five.Relay(25);
 
-       let stepper_X = new five.Stepper({
-          type: five.Stepper.TYPE.DRIVER,
-          stepsPerRev: 500,
-          pins: {
-            step: 54,
-            dir: 55
-        }
-    });
-    let stepper_Y = new five.Stepper({
-          type: five.Stepper.TYPE.DRIVER,
-          stepsPerRev: 500,
-          pins: {
-            step: 26,
-            dir: 28
-        }
-    });
-    let stepper_Z = new five.Stepper({
-          type: five.Stepper.TYPE.DRIVER,
-          stepsPerRev: 200,
-          pins: {
-            step: 36,
-            dir: 34
-        }
-    });
+            const fertilizerPumprelay = new five.Relay(27);
 
-    let ledR = new five.Led(17);
-    let ledG = new five.Led(23);
-    let ledB = new five.Led(16);
+            stepperX.rpm(180).cw()
+                .step(x, function() {
+                    console.log("x done");
+                });
 
-    let waterPumprelay = new five.Relay(25);
+            stepperY.rpm(180).cw()
+                .step(y, function() {
+                    console.log("y done");
+                });
 
-    let fertilizerPumprelay = new five.Relay(27);
+            stepperZ.rpm(180).cw()
+                .step(z, function() {
+                    console.log("z done");
+                });
 
+            if (water === "On") {
+                waterPumprelay.on();
+            }
 
-        stepper_X.rpm(180).cw().step(x, function() {
-          console.log("x done");
-      });
-        stepper_Y.rpm(180).cw().step(y, function() {
-          console.log("y done");
-      });
-        stepper_Z.rpm(180).cw().step(z, function() {
-          console.log("z done");
-      });
+            board.wait(1000, function() {
+                waterPumprelay.off();
+                console.log("water off");
+            });
 
-  		if(water == "On"){
+            if (fertilizer === "On") {
+                fertilizerPumprelay.on();
+            }
 
-			waterPumprelay.on();
-		  }
-		  board.wait(1000, function(){
+            board.wait(1000, function() {
+                fertilizerPumprelay.off();
+                console.log("fertilizer off");
+            });
 
-			waterPumprelay.off();
-			console.log("water off")
-		});
-		if(fertilizer == "On"){
-
-			fertilizerPumprelay.on();
-		  }
-		  board.wait(1000, function(){
-
-			fertilizerPumprelay.off();
-			console.log("fertilizer off")
-		});
-		if(LED == "On"){
-
-			ledR.on();
-  			ledG.on();
-  			ledB.on();
-		}
+            if (LED === "On") {
+                ledR.on();
+                ledG.on();
+                ledB.on();
+            }
 
         // servos.x = new five.Servo({pin: 4, startAt:90,range:[70,130]});
 
@@ -136,19 +135,16 @@ function go(point , water, fertilizer, LED) {
   		button.on("up", function() {
    			console.log("up");
         });*/
-
+        });
     });
-});
 }
 
 
 const xyzinput = {
-  x: 10,
-  y: 20,
-  z: 30
+    x: 10,
+    y: 20,
+    z: 30
 };
-a(xyzinput, "off", "off", "off");
-
-
+go(xyzinput, "off", "off", "off");
 
 
--- node/index.js
+++ node/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 app.get("/", function(req, res) {
     res.send("arduino資訊網頁");
 });
@@ -10,13 +9,13 @@ app.get("/th", function(req, res) {
     const humid = req.query.airhumidity; // 讀取查詢字串的humid值
     const soidhumid = req.query.soidmoisture;// 讀取查詢字串的humid值
     const ph = req.query.ph;// 讀取查詢字串的humid值
-    const waterLevel = req.query.waterLevel;// 讀取查詢字串的humid值
-
+    const waterLevelTank1 = req.query.waterLevelTank1;// 讀取查詢字串的第一水箱水位值
+    const waterLevelTank2 = req.query.waterLevelTank2;// 讀取查詢字串的第二水箱水位值
     // 確認有收到溫度和濕度值（兩者都不是undefined）
     if (temp !== undefined && humid !== undefined) {
         //印出值
-        console.log("溫度 :" + temp + "，濕度 :" + humid + ",土地濕度 :" + soidhumid + " PH值 :" + ph + " 水位 :" + waterLevel);
-        res.send("溫度: " + temp + "°C，濕度： " + humid + " % ,土地濕度 " + soidhumid + "PH值" + ph + "水位" + waterLevel);
+        console.log("溫度 :" + temp + "，濕度 :" + humid + ",土地濕度 :" + soidhumid + " PH值 :" + ph + " 水位1:" + waterLevelTank1 + " 水位2:" + waterLevelTank2);
+        res.send("溫度: " + temp + "°C，濕度： " + humid + " % ,土地濕度 " + soidhumid + "PH值" + ph + "水位1:" + waterLevelTank1 + " 水位2:" + waterLevelTank2);
     } else {
         console.log("沒收到資料！");
     }
