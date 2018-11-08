c24c6614a6f964fcde9ff4c090732a115bf4d311
SoftwareSing <SoftwareSing@gmail.com>
2018-11-08 17:28:39 +0800

    嘗試使用 toggle

--- node/control/detail/_controlFertilizerPump.js
+++ node/control/detail/_controlFertilizerPump.js
@@ -4,12 +4,12 @@ export function _controlFertilizerPump({state, time}, board, callback) {
     const fertilizerPumprelay = new five.Relay(27);
 
     if (state === true) {
-        fertilizerPumprelay.close();
+        fertilizerPumprelay.toggle();
         console.log(`fertilizer: ${fertilizerPumprelay.isOn}`);
     }
 
     const offFun = () => {
-        fertilizerPumprelay.open();
+        fertilizerPumprelay.toggle();
         console.log("fertilizer off");
 
         if (typeof callback === "function") {
