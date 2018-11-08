c27f96dd93e06cd0462fb8efc8e2bb51211e296b
SoftwareSing <SoftwareSing@gmail.com>
2018-11-08 17:21:05 +0800

    將 Relay 的on off 改為使用 open close

--- node/control/detail/_controlFertilizerPump.js
+++ node/control/detail/_controlFertilizerPump.js
@@ -4,12 +4,12 @@ export function _controlFertilizerPump({state, time}, board, callback) {
     const fertilizerPumprelay = new five.Relay(27);
 
     if (state === true) {
-        fertilizerPumprelay.on();
+        fertilizerPumprelay.open();
         console.log(`fertilizer: ${fertilizerPumprelay.isOn}`);
     }
 
     const offFun = () => {
-        fertilizerPumprelay.off();
+        fertilizerPumprelay.close();
         console.log("fertilizer off");
 
         if (typeof callback === "function") {
