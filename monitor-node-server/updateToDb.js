96748b928faf89d2f47be0249eda694438e35469
SoftwareSing <SoftwareSing@gmail.com>
2018-11-11 17:54:52 +0800

    讓監測server自動傳資料到firebase
    記得要在config資料夾內放入config檔

---
+++ monitor-node-server/updateToDb.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+import { firebaseConfig } from "@/firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+export function updateToDb(data) {
+    const time = Date.now();
+    db.ref(`/monitorData/${time}`).set(data);
+}
