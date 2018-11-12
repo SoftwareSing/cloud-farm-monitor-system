644c89276fba75b646d0abf156f5452507c888d8
SoftwareSing <SoftwareSing@gmail.com>
2018-11-12 21:38:33 +0800

    修改控制pump的code
    將2個pump都改為使用 l298
    因不明原因觸發 stop() 時才會抽水， forward() 會停止抽水

--- control-node-server/control/detail/_controlFertilizerPump.js
+++ control-node-server/control/detail/_controlFertilizerPump.js
@@ -3,28 +3,24 @@ const five = require("johnny-five");
 export function _controlFertilizerPump({state, time}, board, callback) {
     const fertilizerPumpMotor = new five.Motor({
         pins: {
-            pwm: 11,
-            dir: 25
+            pwm: 6,
+            dir: 27
         }
     });
 
-    board.repl.inject({
-        motor: fertilizerPumpMotor
-    });
-
     if (state === true) {
-        fertilizerPumpMotor.forward(255);
+        fertilizerPumpMotor.stop();
         console.log(`fertilizer (motor): forward`);
     }
 
-    const offFun = () => {
-        fertilizerPumpMotor.stop();
+    const offFunction = () => {
+        fertilizerPumpMotor.forward(255);
         console.log(`fertilizer (motor): stop`);
 
         if (typeof callback === "function") {
             callback();
         }
     };
-    setTimeout(offFun, time);
+    setTimeout(offFunction, time);
     // board.wait(time, offFun);
 }
