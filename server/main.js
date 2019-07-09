3f31ec9496fd8e1503f90e2104ea4e4cb81b0a9d
SoftwareSing <SoftwareSing@gmail.com>
2019-07-09 16:45:48 +0800

    提供client網頁

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+
 import { addControlApi } from "~/http/control.api";
 import { addMonitorApi } from "~/http/monitor.api";
 
@@ -8,6 +10,8 @@ app.use(express.json());
 const apiBaseUrl = "/api";
 addControlApi(app, apiBaseUrl);
 addMonitorApi(app, apiBaseUrl);
+
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 app.listen(5438, function() {
     console.log("start on 5438");
